01ee8b0201a11e82820f18035563b43959b10cd5
RyUkApples <cocoetmathieudu86@gmail.com>
2017-07-04 16:08:33 +0200

    Base Features #2

--- darkrp/gamemode/config/config.lua
+++ darkrp/gamemode/config/config.lua
@@ -44,7 +44,19 @@ config.communityname = "YOURCOMMUNTIY"
 config.siteweb = "WWW.YOURSITE.COM"
 -- Currency
 config.currency = "$"
+-- define the first spawn position/radius of the player (very first spawn on the server, or after death)
+config.spawn_enabled = false -- set to false to disable the feature
 -- First Spawn Coords
-config.firstSpawnCoords = {x=464.091, y=-997.166, z=24.915}
+config.spawn_position = {1854.21, 3685.51, 34.2671}
+-- radius of spawn
+config.spawn_radius = 1
+-- mp_m_freemode_01 (male) or mp_f_freemode_01 (female)
+config.default_customization = {
+  model = "mp_m_freemode_01" 
+}
+-- init default ped parts
+for i=0,19 do
+  config.default_customization[i] = {0,0}
+end
 
 return config
