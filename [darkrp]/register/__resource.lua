01f71222bb9b3ade9556ab758b3bbaa1b2b54647
RyUkApples <cocoetmathieudu86@gmail.com>
2017-07-06 16:27:36 +0200

    Revert "Revert "Add register""
    This reverts commit e110809df80cb8edaf820df13595839388af5d11.

---
+++ [darkrp]/register/__resource.lua
@@ -0,0 +1,34 @@
+client_script 'client/client.lua'
+client_script 'client/cl_out.lua'
+
+server_script 'client/server.lua'
+
+ui_page 'client/html/index.html'
+
+files {
+	'client/html/index.html',
+	'client/html/script.js',
+	'client/html/style.css',
+	'client/html/cursor.png',
+	'client/html/img/14Street3.png',
+	'client/html/img/14Street4.png',
+	'client/html/img/14Street5.png',
+	'client/html/img/15Standard8.png',
+	'client/html/img/15Standard11.png',
+	'client/html/img/15Standard12.png',
+	'client/html/img/15Standard17.png',
+	'client/html/img/15Standard19.png',
+	'client/html/img/35Lowriders4.png',
+	'client/html/img/39Hipster1.png',
+	'client/html/img/39Hipster6.png',
+	'client/html/img/39Hipster10.png',
+	'client/html/img/Casual1.png',
+	'client/html/img/Casual3.png',
+	'client/html/img/party2.png',
+	'client/html/img/2Beach2.png',
+	'client/html/img/2Beach7.png',
+	'client/html/img/2Beach8.png',
+	'client/html/img/Femme1.png',
+	'client/html/img/Femme2.png',
+	'client/html/img/Femme3.png'
+}
